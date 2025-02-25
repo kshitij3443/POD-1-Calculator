ce59927dca4949fdadc4c802de4ef3827a460582
Suraj Kumar <suraj.kumar2@publicissapient.com>
2025-02-25 13:31:42 +0530

    Added the addition functionality in the calculator

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,13 @@
 public class Calculator{
+    public static int add(int firstNumber, int secondNumber){
+        return firstNumber+secondNumber;
+    }
+
     public static void main(String[] args) {
-        
+        int firstNumber = 2;
+        int secondNumber = 5;
+
+        int addedNumber  = add(firstNumber, secondNumber);
+        System.out.println("The added number is: " + addedNumber);
     }
 }
