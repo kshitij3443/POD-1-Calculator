c8cd1a5ef1a15e1c25a44f990468aaf1bdefb485
Kshitij Jaiswal <kshitij.jaiswal@publicissapient.com>
2025-02-25 13:54:02 +0530

    code refactor

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,20 @@
 public class Calculator{
-    public static int add(int firstNumber, int secondNumber){
-        return firstNumber+secondNumber;
+
+    public static int add(int a, int b){
+        return a+b;
     }
 
+    public static int subtract(int a, int b){
+        return a-b;
+    }
     public static void main(String[] args) {
         int firstNumber = 2;
         int secondNumber = 5;
 
         int addedNumber  = add(firstNumber, secondNumber);
         System.out.println("The added number is: " + addedNumber);
+
+        int subtractedNumber  = subtract(addedNumber, addedNumber);
+        System.out.println("The subtracted number is: " + subtractedNumber);
     }
 }
